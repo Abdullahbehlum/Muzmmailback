c83a92f573cbd7c7b538ba1184da7a0e1dfe1a4f
Abdullah <139277467+Abdullahbehlum@users.noreply.github.com>
2024-01-30 16:11:51 +0500

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -3,7 +3,14 @@ const dotenv = require("dotenv").config();
 const ConnectDb = async () => {
   try {
     const dbUrl = process.env.MONGO_URL;
-     await mongoose.connect(dbUrl);
+     await mongoose.connect(dbUrl ,     {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 30000,
+        socketTimeoutMS: 30000,
+        poolSize: 10, 
+    }
+);
     console.log("Database connection established");
   } catch (error) {
     await console.log(error.message);
