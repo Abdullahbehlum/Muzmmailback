2a76d483a0f7aa7c6a8daa422b12b8056774598d
Abdullah <139277467+Abdullahbehlum@users.noreply.github.com>
2024-01-30 15:38:37 +0500

    Update AddProductApi.js

--- controllers/api/insert/AddProductApi.js
+++ controllers/api/insert/AddProductApi.js
@@ -34,7 +34,7 @@ route.post("/",  upload.any('images') ,async (req, res) => {
 })
 route.get("/", async (req, res) => {
     try {
-        const product = await AddProductApi.find().lean();
+        const product = await AddProductApi.find().lean().exec();
         res.status(201).json({ msg: 'product get successfully', products: product })
     } catch (error) {
         res.status(500).json({ msg: 'Product Add failed', error: error.message })
